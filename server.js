e84e51e94b7fe094a4fcde7f9df2c472f7505d2e
hameed kazeem <hameed.kazeem@gmail.com>
2020-10-05 04:17:37 -0400

    update app routes on server for heroku

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));//this helps us serve all our assets without having to create routes for each one
-app.use('/api',routes);
-app.use('/', htmlRoutes);
+app.use('/',routes);
 app.listen(PORT, () => {
     console.log(`App is now listening on port ${PORT}`);
 })
